c9faf3c0720bae54897ddcf119564831314c782a
Alef Felix <alef.developerweb@gmail.com>
2018-10-15 17:55:48 -0300

    Adicionando conexão ao mongodb

--- dist/app.js
+++ dist/app.js
@@ -2,19 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const crmRoutes_1 = require("./routes/crmRoutes");
 class App {
     constructor() {
         this.routePrv = new crmRoutes_1.Routes();
+        this.mongoUrl = 'mongodb://admin:master123@ds033187.mlab.com:33187/nodestr2';
         this.app = express();
         this.config();
         this.routePrv.routes(this.app);
+        this.mongoSetup();
     }
     config() {
         // support application/json type post data
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+    mongoSetup() {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 }
 exports.default = new App().app;
--- lib/app.ts
+++ lib/app.ts
@@ -1,16 +1,19 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
 import { Routes } from "./routes/crmRoutes";
 
 class App{
 
     public app: express.Application;
     public routePrv: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://admin:master123@ds033187.mlab.com:33187/nodestr2'
 
     constructor(){
         this.app = express();
         this.config();
         this.routePrv.routes(this.app);
+        this.mongoSetup();
     }
 
     private config(): void{
@@ -19,6 +22,11 @@ class App{
         
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 
 }
